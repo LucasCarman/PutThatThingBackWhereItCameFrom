43ddade3146bf843e5dfaf833d9d53bab6355d6d
Lucas Carman <lcarm014@fiu.edu>
2021-12-09 22:11:52 -0500

    Created classes

---
+++ PutThatThingBackWhereItCameFrom/src/AntiMalware/Scanner.java
@@ -0,0 +1,61 @@
+package AntiMalware;
+
+public abstract class Scanner {
+	
+	private int ScanSpeed = 1;
+	private int ScanSchedule;
+	private int FileCode;
+	
+	public Scanner()
+	{
+		
+	}
+	
+	private void DisplayScanOptions()
+	{
+		
+	}
+	
+	private void SelectScanType()
+	{
+		
+	}
+	
+	private void DisplayScanSchedule()
+	{
+		
+	}
+	
+	private int SetScanSchedule()
+	{
+		return ScanSchedule;
+		
+	}
+	
+	private void TriggerAction()
+	{
+		
+	}
+	
+	private void Abort()
+	{
+		
+	}
+	
+	private void SelectHeirarchy()
+	{
+		
+	}
+	
+	private int FileCodeCollected()
+	{
+		return FileCode;
+		
+	}
+	
+	private void CodeAnalyzed()
+	{
+		
+	}
+
+}
